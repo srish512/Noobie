9285b40d02e16ae5cec536f738b18db2fcd2d975
srish512 <srish512@gmail.com>
2018-08-25 20:18:57 +0530

    first commit

---
+++ workspace/myMavenProject/src/test/java/com/org/learningMaven/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com.org.learningMaven;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest
+{
+@Test
+public void Login()
+{
+	System.out.println("Hello Shreenidhi Rai");
+}
+@Test
+public void Login1()
+{
+	System.out.println("Hello Saryabeedu");
+}
+}
---
+++ workspace/myMavenProject/src/test/java/com/org/learningMaven/Normal.java
@@ -0,0 +1,9 @@
+package com.org.learningMaven;
+
+public class Normal {
+
+	public static void main(String args[])
+	{
+		System.out.println("Normal test");
+	}
+}
